41dd51ed98c913db2b4ab8496feddf0fb0ee7571
Pavol Hejný <me@pavolhejny.com>
2021-06-11 14:10:49 +0000

    All invisible elements are virtual

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { forTime, forAnimationFrame } from "https://cdn.skypack.dev/waitasecond@1.6.0";
+import { Editor } from "./classes/Editor.js";
 
 export async function main() {
+
+
 
     if (!navigator.mediaDevices.getUserMedia) {
         alert(`Problem with your camera! Please try to use newest browser and device with webcam or mobile phone.`);
@@ -9,8 +12,14 @@ export async function main() {
 
 
 
-    const videoElement = document.getElementById('videoElement');
+    const videoElement = document.createElement('video');
+    videoElement.setAttribute('autoPlay', true);
     const sceneElement = document.getElementById('scene');
+    const sceneContext = sceneElement.getContext('2d')
+    const maskElement = document.createElement('canvas');
+    const maskContext = maskElement.getContext('2d')
+
+
 
     let stream;
     try {
@@ -30,11 +39,20 @@ export async function main() {
 
     sceneElement.width = videoElement.videoWidth;
     sceneElement.height = videoElement.videoHeight;
+    maskElement.width = videoElement.videoWidth;
+    maskElement.height = videoElement.videoHeight;
+
+    const editor = new Editor(sceneElement, maskContext);
+
 
 
     while (true) {
         await forAnimationFrame();
-        sceneElement.getContext('2d').drawImage(videoElement, 0, 0);
+        sceneContext.globalCompositeOperation = 'source-over';
+        sceneContext.drawImage(videoElement, 0, 0);
+        sceneContext.globalCompositeOperation = 'destination-out';
+
+        sceneContext.drawImage(maskElement, 0, 0);
     }
 
 
