fb2d550b5f058f1f69a912416bffd227ded26e08
Pavol Hejný <me@pavolhejny.com>
2021-06-11 12:15:46 +0000

    Better error handling

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 
 export async function main() {
 
+    if (!navigator.mediaDevices.getUserMedia) {
+        alert(`Problem with your camera! Please try to use newest browser and device with webcam or mobile phone.`);
+        return;
+    }
+
+
 
     const videoElement = document.getElementById('videoElement');
 
-    if (navigator.mediaDevices.getUserMedia) {
-        videoElement.srcObject = await navigator.mediaDevices.getUserMedia({ video: true });
+    let stream;
+    try {
+        stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    } catch (error) {
+        alert(`Problem with your camera! Your camera is probbably in use.`);
+        throw error;
     }
+
+
+    console.log({ stream })
+    videoElement.srcObject = stream;
+
 
 
 
