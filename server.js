1dcc5c8bcf7d3534a875a4d08c63ffb930bd7585
Pavol Hejný <me@pavolhejny.com>
2021-06-11 11:50:33 +0000

    Exposing server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')  
+const serveStatic = require('serve-static')
+const serveIndex = require('serve-index')
+const open = require('open')
+
+
+const PORT = 3000;
+
+const staticBasePath = './';
+
+const app = express();
+
+app.use(serveStatic(staticBasePath, {'index': false}))  
+app.use(serveIndex(staticBasePath, {'icons': true}))  
+app.listen(PORT);
+
+console.log(`Static server listening on port ${PORT}.`);
+open(`http://localhost:${PORT}/samples/`);
